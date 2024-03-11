a49f3576f98eb47996f024821fb183edb3cc2597
Maider Landaribar <maiderlandaribar@Maiders-MacBook-Pro.local>
2024-03-11 11:12:19 +0100

    test: check if radio buttons exist

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,8 +1,11 @@
 // @ts-check
 const { test, expect } = require("@playwright/test");
 
-test("has title", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:5500/");
+});
+
+test("has title", async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(
     page.getByRole("heading", {
@@ -10,3 +13,28 @@ test("has title", async ({ page }) => {
     })
   ).toBeVisible();
 });
+
+test("has begginer", async ({ page }) => {
+  await page.getByLabel("Principiante").check();
+});
+
+test("has medium", async ({ page }) => {
+  await page.getByLabel("Medio").check();
+});
+
+test("has expert", async ({ page }) => {
+  await page.getByLabel("Experto").check();
+});
+/*
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
+});
+*/
